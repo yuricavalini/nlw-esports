4a21e447a5a691b8dde55ce24fbebb031b383c2e
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-12 14:44:04 -0300

    feat: add example buttons

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Button title='Send 1' />
+      <Button title='Send 2' />
+      <Button title='Send 3' />
+      <Button title='Hello World' />
+
+      <StatusBar style="auto"  />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
