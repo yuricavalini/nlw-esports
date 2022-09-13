ca1c82c2aef1fbd857bbd7eb93c0bf3815797bf3
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-13 20:03:18 -0300

    feat: add Background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Button title='Send 1' />
-      <Button title='Send 2' />
-      <Button title='Send 3' />
-      <Button title='Hello World' />
-
-      <StatusBar style="auto"  />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
   );
 }
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
