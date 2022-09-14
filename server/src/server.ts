6edcf781e8601141bc1c17e5b5d02016110b640e
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-14 08:04:24 -0300

    feat: add games list route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
 app.get('/ads', (request, response) => {
   return response.send('Acessou Ads!')
 })
