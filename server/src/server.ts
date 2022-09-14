056cbfa14aa12116edaf02a60137fb645bb1b979
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-14 13:56:04 -0300

    feat: add database consult for games list

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,23 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/games', (request, response) => {
-  return response.json([])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
 })
 
 app.post('/ads', (request, response) => {
