dbb2726547d381315e341cabd5a9dde047f60718
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-13 21:05:58 -0300

    feat: add FlatList component of GameCard component

--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -10,6 +10,10 @@ export const styles = StyleSheet.create({
     width: 214,
     height: 120,
     marginTop: 74,
-    marginBottom: 48
+    marginBottom: 48,
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
   }
 });
