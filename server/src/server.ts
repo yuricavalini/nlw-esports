24378dbf78662815ef80387cf8544a67b0ceadb1
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-14 20:56:37 -0300

    feat(server): add games ads creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
 
 const app = express()
+
+app.use(express.json())
+
 const prisma = new PrismaClient({
   log: ['query']
 })
@@ -20,8 +24,26 @@ app.get('/games', async (request, response) => {
   return response.json(games)
 })
 
-app.post('/ads', (request, response) => {
-  return response.status(201).json([])
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+  const body = request.body
+  
+  // Adicionar Validação - Zod
+  
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    }
+  })
+
+  return response.status(201).json(ad)
 })
 
 app.get('/games/:id/ads', async (request, response) => {
