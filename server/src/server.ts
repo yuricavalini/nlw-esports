07498bc3403a8f04c2cab1a0fd044ce8615b0179
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-14 21:04:04 -0300

    fix(server): convert hourStart and hourEnd in game ads route using convertMinutesToHourString utility function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 const app = express()
 
@@ -70,7 +71,9 @@ app.get('/games/:id/ads', async (request, response) => {
   return response.json(ads.map((ad) => {
     return {
       ...ad,
-      weekDays: ad.weekDays.split(',')
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd)
     }
   }))
 })
