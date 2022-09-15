b7120933da03430e4b39c87e304b2bb8dda9f98a
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-15 08:01:09 -0300

    feat(web): add CreateAdBanner component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import { GameBanner } from './components/GameBanner'
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { CreateAdBanner } from './components/CreateAdBanner'
 
 import "./styles/main.css"
 
@@ -23,19 +23,7 @@ function App() {
         <GameBanner bannerUrl='/game-6.png' title='Fortnite' adsCount={4} />
       </div>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
 
   )
